24cd2d213ae2af1afeeaa789eadaf921168bee22
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-01-19 14:00:07 +0000

    FluentValidation

--- src/Altkom.Orange.Models.Validators/CustomerValidator.cs
+++ src/Altkom.Orange.Models.Validators/CustomerValidator.cs
@@ -1,4 +1,5 @@
-﻿using FluentValidation;
+﻿using Altkom.Orange.IServices;
+using FluentValidation;
 using System;
 
 namespace Altkom.Orange.Models.Validators
@@ -8,19 +9,36 @@ namespace Altkom.Orange.Models.Validators
 
     public class CustomerValidator : AbstractValidator<Customer>
     {
-        public CustomerValidator()
+        private readonly ICustomerService customerService;
+
+        public CustomerValidator(ICustomerService customerService)
         {
+            this.customerService = customerService;
+
             RuleFor(p => p.LastName).NotEmpty().Length(3, 50);
             RuleFor(p => p.Email).EmailAddress().NotEmpty();
             RuleFor(p => p.CreditAmount).InclusiveBetween(1, 1000);
-            RuleFor(p => p.Pesel).Must(Validate);
+            RuleFor(p => p.Pesel)
+                .Must(ValidateChecksum).WithMessage(customer => $"Niepoprawny PESEL {customer.Pesel}")
+                .Must(ValidateExists).WithMessage("Użytkownik o podanym numerze PESEL już istnieje");
             RuleFor(p => p.CreditAmount).NotEqual(0).When(p => !p.IsRemoved);
           //  RuleFor(p => p).Custom((c, context)=> context.)
         }
-        
-        private bool Validate(string pesel)
+
+
+        private bool ValidateChecksum(string pesel)
         {
             return pesel.Length == 11;
+        }
+
+        private bool ValidateExists(string pesel)
+        {
+            Customer customer = customerService.Get(pesel);
+
+            if (customer != null)
+                return false;
+
+            return true;
         }
 
     }
