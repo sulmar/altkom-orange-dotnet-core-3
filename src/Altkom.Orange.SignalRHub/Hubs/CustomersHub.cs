6f076285ff06e54a9e64c8fef982b74bc811006b
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-01-20 14:30:58 +0000

    SignalR Hub

---
+++ src/Altkom.Orange.SignalRHub/Hubs/CustomersHub.cs
@@ -0,0 +1,22 @@
+﻿using Altkom.Orange.Models;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Altkom.Orange.SignalRHub.Hubs
+{
+    public class CustomersHub : Hub
+    {
+        public override Task OnConnectedAsync()
+        {
+            return base.OnConnectedAsync();
+        }
+
+        public async Task SendNewCustomer(Customer customer)
+        {
+            await Clients.All.SendAsync("YouHaveGotNewCustomer", customer);
+        }
+    }
+}
