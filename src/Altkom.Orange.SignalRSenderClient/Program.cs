82cb8347ae798be57d84102e0d6114a20602e89b
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-01-20 15:43:02 +0000

    Obsluga bledow

--- src/Altkom.Orange.SignalRSenderClient/Program.cs
+++ src/Altkom.Orange.SignalRSenderClient/Program.cs
@@ -2,6 +2,8 @@
 using Altkom.Orange.Models;
 using Microsoft.AspNetCore.SignalR.Client;
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Altkom.Orange.SignalRSenderClient
@@ -20,18 +22,48 @@ namespace Altkom.Orange.SignalRSenderClient
             // dotnet add package Microsoft.AspNetCore.SignalR.Client
             HubConnection connection = new HubConnectionBuilder()
                 .WithUrl(url)
+                .WithAutomaticReconnect(new[] { TimeSpan.Zero, TimeSpan.FromSeconds(5), TimeSpan.FromSeconds(30) })
                 .Build();
+
+            // js
+            // @microsoft/signalr
+
+            connection.Closed += error =>
+            {
+                if (connection.State == HubConnectionState.Disconnected)
+                {
+                    Console.WriteLine("Utracono polaczenie");
+                }
+
+                return Task.CompletedTask;
+            };
+
+            connection.Reconnecting += error =>
+            {
+                if (connection.State == HubConnectionState.Reconnecting)
+                {
+                    Console.WriteLine("Reconnecting...");
+                }
+
+                return Task.CompletedTask;
+            };
 
             Console.WriteLine("Connecting...");
             await connection.StartAsync();
             Console.WriteLine("Connected.");
 
             CustomerFaker customerFaker = new CustomerFaker();
-            Customer customer = customerFaker.Generate();
 
-            Console.WriteLine($"Send {customer.FirstName} {customer.LastName}");
-            await connection.SendAsync("SendNewCustomer", customer);
-            Console.WriteLine($"Sent.");
+            IEnumerable<Customer> customers = customerFaker.GenerateForever();  // yield
+
+            foreach (Customer customer in customers)
+            {
+                Console.WriteLine($"Send {customer.FirstName} {customer.LastName}");
+                await connection.SendAsync("SendNewCustomer", customer);
+                Console.WriteLine($"Sent.");
+
+                await Task.Delay(TimeSpan.FromSeconds(0.1));
+            }
 
             Console.WriteLine("Press any key to exit.");
             Console.ReadKey();
