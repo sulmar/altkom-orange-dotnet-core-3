ef946422ceff09f3e35f1e75def3f2d28da0f886
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-01-20 14:31:06 +0000

    SignalR Client

---
+++ src/Altkom.Orange.SignalRSenderClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Altkom.Orange.SignalRSenderClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Sender Signal-R!");
+        }
+    }
+}
