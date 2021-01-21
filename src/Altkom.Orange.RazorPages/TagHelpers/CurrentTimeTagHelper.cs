445d040e657848e9e522e3410e229955e32da7f6
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-01-21 13:36:16 +0000

    Tag-Helpers

---
+++ src/Altkom.Orange.RazorPages/TagHelpers/CurrentTimeTagHelper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Altkom.Orange.RazorPages.TagHelpers
+{
+    public class CurrentTimeTagHelper : TagHelper
+    {
+        public string Title { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "strong";
+
+            output.Content.SetHtmlContent($"{Title} <b>{DateTime.Now}</b>");
+        }
+    }
+}
