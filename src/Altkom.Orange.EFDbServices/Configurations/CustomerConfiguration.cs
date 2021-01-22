2aa6551066e630da07565485dcc7cfa4775792fd
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-01-22 14:43:21 +0000

    EF Core

---
+++ src/Altkom.Orange.EFDbServices/Configurations/CustomerConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Altkom.Orange.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Altkom.Orange.EFDbServices.Configurations
+{
+    public class CustomerConfiguration : IEntityTypeConfiguration<Customer>
+    {
+        public void Configure(EntityTypeBuilder<Customer> builder)
+        {
+
+            builder.Property(p => p.FirstName)
+                .HasMaxLength(50)
+                .IsRequired();
+
+            builder.Property(p => p.LastName)
+               .HasMaxLength(50)
+               .IsRequired();
+
+            builder.Property(p => p.Pesel)
+               .HasMaxLength(11)
+               .IsUnicode(false)
+               .IsRequired();
+        }
+    }
+}
--- src/Altkom.Orange.EFDbServices/OrangeContext.cs
+++ src/Altkom.Orange.EFDbServices/OrangeContext.cs
@@ -1,4 +1,5 @@
-﻿using Altkom.Orange.Models;
+﻿using Altkom.Orange.EFDbServices.Configurations;
+using Altkom.Orange.Models;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -17,5 +18,36 @@ namespace Altkom.Orange.EFDbServices
         public DbSet<Order> Orders { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<Service> Services { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // modelBuilder.Entity<Customer>().HasKey(p => p.Id).HasName("_CustomerId");
+
+            // modelBuilder.Entity<Customer>().ToTable("CRM.Klienci");
+
+            // modelBuilder.Entity<Customer>().HasIndex(p=>p.Pesel).ForSqlServerIsClustered();
+
+            //modelBuilder.Entity<Customer>().Property(p => p.FirstName)
+            //    .HasMaxLength(50)
+            //    .IsRequired();
+
+            //modelBuilder.Entity<Customer>().Property(p => p.LastName)
+            //   .HasMaxLength(50)
+            //   .IsRequired();
+
+            //modelBuilder.Entity<Customer>().Property(p => p.Pesel)
+            //   .HasMaxLength(11)
+            //   .IsUnicode(false)
+            //   .IsRequired();
+
+           // modelBuilder.ApplyConfiguration(new CustomerConfiguration());
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(CustomerConfiguration).Assembly);
+
+            modelBuilder.Entity<Order>().Property(p => p.OrderDate).HasColumnType("datetime");
+
+        }
     }
 }
